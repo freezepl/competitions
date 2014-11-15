58ba76841940e1ca03709bf67249a408d08f2c7c
Lukasz Mlodzik <lukasz.mlodzik@gmail.com>
2014-11-15 17:49:55 +0100

    Created competition scaffold

---
+++ app/controllers/competitions_controller.rb
@@ -0,0 +1,60 @@
+class CompetitionsController < ApplicationController
+  before_action :set_competition, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @competitions = Competition.all
+  end
+
+  def show
+  end
+
+  def new
+    @competition = Competition.new
+  end
+
+  def edit
+  end
+
+  def create
+    @competition = Competition.new(competition_params)
+
+    respond_to do |format|
+      if @competition.save
+        format.html { redirect_to @competition, notice: 'Competition was successfully created.' }
+        format.json { render :show, status: :created, location: @competition }
+      else
+        format.html { render :new }
+        format.json { render json: @competition.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @competition.update(competition_params)
+        format.html { redirect_to @competition, notice: 'Competition was successfully updated.' }
+        format.json { render :show, status: :ok, location: @competition }
+      else
+        format.html { render :edit }
+        format.json { render json: @competition.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @competition.destroy
+    respond_to do |format|
+      format.html { redirect_to competitions_url, notice: 'Competition was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_competition
+      @competition = Competition.find(params[:id])
+    end
+
+    def competition_params
+      params.require(:competition).permit(:name, :year)
+    end
+end
---
+++ app/views/competitions/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @competition, :id, :name, :year, :created_at, :updated_at
